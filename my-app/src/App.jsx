06f5e7e577fe78dcca9db1ab1dc6dba36d18c5d1
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-11-05 21:52:17 +0100

    excercise

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,18 @@
 import { HelloWorld } from "./helloWorld";
+import { Message } from "./message";
+
 
 export function App(){
    return (
     <div>
       <h1>This is my app</h1>
       <HelloWorld/>
+      <HelloWorld/>
+      <HelloWorld/>
+      <HelloWorld/>
+      <Message/>
     </div>
    )
 }
+/* si possono ripetere piu volte il richiamo della funzione Hello e me lo ristampa piu volte in pagina e invece la funzione Message viene messo nel div di app
+ */
--- my-app/src/helloWorld.jsx
+++ my-app/src/helloWorld.jsx
@@ -3,6 +3,5 @@ import { Message } from "./message";
 export function HelloWorld(){
   return (<div>
     <h2>Hello, world!</h2>
-    <Message />
     </div>)
 }
