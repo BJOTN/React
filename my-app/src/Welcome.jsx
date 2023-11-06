5f36684429303adf4aa193a1deab1db5a838087e
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-11-06 15:28:44 +0100

    excercise Conditional rendering

--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -3,8 +3,12 @@ import { Age } from "./Age";
 export function Welcome({ name, age }) {
   return (
     <div>
-      <p>Welcome {name}!</p>;
+      {<p>Welcome {name}!</p>}
       <Age age={age} />
+      {age > 18 && <Age age={age} />}
+      {age && <Age age={age} />}
+      {age > 18 && Age < 65 && <Age age={age} />}
+      {age> 18  && name === 'John' && <Age age={age} />}
     </div>
   );
 }
