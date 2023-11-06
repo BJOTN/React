6ae8691d490b0c0f0f98603d25880115a6d8820f
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-11-06 12:26:15 +0100

    excercie prop

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,10 @@
 import { Welcome } from "./Welcome";
 
 export function App() {
-  return <Welcome name = 'Bjorn'/>;
+  return <Welcome name = 'Bjorn' age ={25}/>;
 }
 
 Welcome.defaultProps = {
   name: "unknow",
+  age: 'none',
 };
-/* -se non passo la prop name in Welcome mi stampa solo welcome senza il nome
-    -mettendo la prop default se non viene specificato il name verrà utilizzato quello di default */
