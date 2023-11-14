966a03620b1278a6595fa57189a9a7a809cc418d
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-11-14 04:29:27 +0100

    fix

--- my-app/src/MouseClicker.jsx
+++ my-app/src/MouseClicker.jsx
@@ -2,12 +2,10 @@ function MouseClicker({ name, nameTwo ,imgUrl}) {
     const handleClick = ((event) => {
         console.log(event.currentTarget.name)
     })
-    const handleClickImg =((event) => {
-        console.log(event.currentTarget.src)
-    })
+    
     return (
         <>
-            <button name={name} onClick={handleClick}><img src={imgUrl} onClick={handleClickImg} /></button>
+            <button name={name} onClick={handleClick}>Click me </button>
             <button name={nameTwo} onClick={handleClick}>Click me</button>
             
         </>)
