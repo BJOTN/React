1e935f00c2c634ea892cc4c66778e077ff3a33b9
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-11-15 22:13:37 +0100

    exercise

--- my-app/src/Login.jsx
+++ my-app/src/Login.jsx
@@ -2,45 +2,51 @@ import { useState } from "react"
 import Welcome from "./Welcome"
 
 function Login({ onLogin }) {
-  const [password, setPassword] = useState("");
-  const [userName, setUserName] = useState("");
-  const [remember, setRemember] = useState(false);
+    const [password, setPassword] = useState("");
+    const [userName, setUserName] = useState("");
+    const [remember, setRemember] = useState(false);
 
-  const handleUserName = (e) => {
-    setUserName(e.target.value);
-  };
+    const handleUserName = (e) => {
+        setUserName(e.target.value);
+    };
 
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-  };
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    };
 
-  const handleRemember = (e) => {
-    setRemember(e.target.checked);
-  };
+    const handleRemember = (e) => {
+        setRemember(e.target.checked);
+    };
 
-  const handleLoginClick = () => {
-    onLogin(userName, password,remember)
-  };
+    const handleLoginClick = (e) => {
+        e.preventDefault()
+        onLogin(userName, password, remember)
+    };
+    const handleReset = () => {
+        setPassword('');
+        setUserName('')
+        setRemember(false);
+    }
+    const isButtonDisabled = !userName || !password;
 
-  const isButtonDisabled = !userName || !password;
+    return (
+        <form onSubmit={handleLoginClick}>
+            <label htmlFor="userName">Username: </label>
+            <input type="text" value={userName} id="userName" onChange={handleUserName} />
 
-  return (
-    <div>
-      <label htmlFor="userName">Username: </label>
-      <input type="text" value={userName} id="userName" onChange={handleUserName} />
+            <label htmlFor="password">Password: </label>
+            <input type="password" value={password} id="password" onChange={handlePassword} />
 
-      <label htmlFor="password">Password: </label>
-      <input type="password" value={password} id="password" onChange={handlePassword} />
+            <label htmlFor="remember">
+                Remember me
+                <input type="checkbox" id="remember" checked={remember} onChange={handleRemember} />
+            </label>
 
-      <label htmlFor="remember">
-        Remember me
-        <input type="checkbox" id="remember" checked={remember} onChange={handleRemember} />
-      </label>
-
-      <button type="button" onClick={handleLoginClick} disabled={isButtonDisabled}>
-        Login
-      </button>
-    </div>
-  );
+            <button type="submit" onClick={handleLoginClick} disabled={isButtonDisabled}>
+                Login
+            </button>
+            <button type="reset" onClick={handleReset} >Reset</button>
+        </form>
+    );
 }
 export default Login
