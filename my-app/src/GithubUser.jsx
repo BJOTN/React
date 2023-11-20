424d700936180e2e724e8ee1fc4a83d030ba5a47
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-11-20 22:29:01 +0100

    exercise

--- my-app/src/GithubUser.jsx
+++ my-app/src/GithubUser.jsx
@@ -2,22 +2,30 @@ import useGithhubUser from "./useGithubUser"
 
 
 export default function GithubUser({ userName }) {
-    const { userData, isLoading } = useGithhubUser(userName);
+    const { fetchUserData, userData, error, loading  } = useGithhubUser(userName);
   
-    return (
-      <div>
-        {isLoading ? (
-          <p>Loading user data...</p>
-        ) : userData ? (
-          <div>
-            <h2>User Details</h2>
-            <img src={userData.avatar_url} alt={`${userData.login}'s avatar`} />
-            <p>Name: {userData.name}</p>
-            <p>Login: {userData.login}</p>
-          </div>
-        ) : (
-          <p>User not found</p>
-        )}
-      </div>
-    );
+    useEffect(() => {
+        if (userName) {
+          fetchUserData(userName);
+        }
+      }, [userName, fetchUserData]);
+    
+      return (
+        <div>
+          {loading ? (
+            <p>Loading user data...</p>
+          ) : error ? (
+            <p>{error}</p>
+          ) : userData ? (
+            <div>
+              <h2>User Details</h2>
+              <img src={userData.avatar_url} alt={`${userData.login}'s avatar`} />
+              <p>Name: {userData.name}</p>
+              <p>Login: {userData.login}</p>
+            </div>
+          ) : (
+            <p>User not found</p>
+          )}
+        </div>
+      );
   }
--- my-app/src/useGithubUser.jsx
+++ my-app/src/useGithubUser.jsx
@@ -1,33 +1,41 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function useGithhubUser(userName) {
     const [userData, setUserData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
-  
-    useEffect(() => {
-      const fetchUserData = async function () {
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+
+    const fetchUserData = async function () {
         try {
-          const response = await fetch(`https://api.github.com/users/${userName}`);
-          if (response.ok) {
-            const data = await response.json();
-            setUserData(data);
-          } else {
-            console.log('error response', response.status);
-          }
+            const response = await fetch(`https://api.github.com/users/${userName}`);
+            if (response.ok) {
+                const data = await response.json();
+                setUserData(data);
+                setError(null)
+            } else {
+                console.log('error response', response.status);
+                setUserData(null)
+                setError(`Error: ${response.status}`);
+            }
         } catch (error) {
-          console.error('error fetching', error);
+            console.error('error fetching', error);
+            setUserData(null);
+            setError("Error fetching user data");
         } finally {
-          setIsLoading(false);
+            setIsLoading(false);
         }
-      };
-  
-      fetchUserData();
-    }, [userName]);
-  
-    return {
-      userData,
-      isLoading,
     };
-  }
-  
-  export default useGithhubUser;
+
+
+    return {
+        fetchUserData,
+        userData,
+        isLoading,
+        error,
+        loading,
+    };
+}
+
+export default useGithhubUser;
