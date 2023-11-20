5b8d5d955c6475d78f69132177c54f1ea0590fe4
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-11-21 00:32:35 +0100

    exercise

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,12 @@
 import { Counter } from "./Counter";
-import { Welcome } from "./Welcome";
+import { Routes, Route } from 'react-router-dom'
 
 export function App() {
   return (
     <div>
-      <h1>Counter App</h1>
-      <Counter initialValue={0} incrementAmount={2} decrementAmount={1} reset ={0}/>
+      <Routes>
+        <Route path='/counter' element={<Counter />} />
+      </Routes>
     </div>
   );
 }
-
-Welcome.defaultProps = {
-  name: "unknow",
-}
-/* -se non passo la prop name in Welcome mi stampa solo welcome senza il nome
-    -mettendo la prop default se non viene specificato il name verrà utilizzato quello di default */
