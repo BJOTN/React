31ebc98d735a9e124248a0a0ecb2db8c19cb4f12
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-12-04 12:23:45 +0100

    fix

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,9 @@
 import { Welcome } from "./Welcome";
 
 export function App() {
-  return <Welcome name = 'Bjorn' age ={25}/>;
+  const name = 'Bjorn';
+  const age = '25'
+  return <Welcome name = {name} age ={age}/>;
 }
 
 Welcome.defaultProps = {
