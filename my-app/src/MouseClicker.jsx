8364be6db781e3a8a35e5dae998ad3f69ddfc5c7
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-12-04 12:37:49 +0100

    excercise

--- my-app/src/MouseClicker.jsx
+++ my-app/src/MouseClicker.jsx
@@ -1,13 +1,27 @@
 function MouseClicker({ name, nameTwo ,imgUrl}) {
-    const handleClick = ((event) => {
-        console.log(event.currentTarget.name)
-    })
-    
-    return (
-        <>
-            <button name={name} onClick={handleClick}>Click me </button>
-            <button name={nameTwo} onClick={handleClick}>Click me</button>
-            
-        </>)
+  const handleClick = (event) => {
+    if (event.currentTarget === event.target) {
+      console.log(event.currentTarget.name);
+    }
+  };
+
+  const handleImageClick = (event) => {
+    if (event.currentTarget.parentElement === event.target.parentElement) {
+      console.log( imgUrl);
+    }
+  };
+
+  return (
+    <>
+      <button name={name} onClick={handleClick}>
+        Click me
+        <img src={imgUrl} alt="Click me" onClick={handleImageClick} />
+      </button>
+      <button name={nameTwo} onClick={handleClick}>
+        Click me
+        <img src={imgUrl} alt="Click me" onClick={handleImageClick} />
+      </button>
+    </>
+  );
 }
 export default MouseClicker
--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom';
 import React from 'react';
-import App from './App';
+import App from './app';
+
 
 
 const root = createRoot(document.getElementById('root'));
