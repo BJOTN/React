e4e010399f56e6f99d1ac3ecc30898651f522dfb
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-12-04 11:06:18 +0100

    fix

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,15 +4,19 @@ import useCurrentLocation from "./useCurrentLocation";
 
 
 function App() {
-  const{location,error,loading,getCurrentPosition}=useCurrentLocation();
+  const { location, error, loading, getCurrentPosition } = useCurrentLocation();
   return (
     <div>
       {loading && <p>Loading</p>}
       {error && <p>Error scemo</p>}
-      {location && <p>{location.latitude}, {location.longitude}</p>}
+      {location && (
+        <p>
+          {location.latitude}, {location.longitude}
+        </p>
+      )}
       <button onClick={getCurrentPosition}>Position</button>
     </div>
-  )
+  );
 }
 
 export default App;
